40bacfeaca6b494bab3d15cac8e21c5b6b31e217
surya21102003 <156424142+surya21102003@users.noreply.github.com>
2025-09-22 19:10:20 +0530

    49768

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from .models import OrderStatus
 # Create your models here.
 class Order(models.model):
-   name=models.CharField(max_lenght=50,unique=True)
+   coded=models.CharField(max_lenght=50,unique=True)
+   discount=models.DecimalField(max_digits=5,decimal_places=2,help_text="discount percentage")
+   created_at=models.DateTimeField(auto_now_add=True)
 
    def __str__(self):
-       return self.name
+       return self.code
 
 
