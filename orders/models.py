0a5a7047023ea498945ca1569b2f9212337d10b8
surya21102003 <156424142+surya21102003@users.noreply.github.com>
2025-09-22 18:55:58 +0530

    49766

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from .models import OrderStatus
 # Create your models here.
+class Order(models.model):
+    status=models.Foreignkey(
+        OrderStatus,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        related_name="orders"
+    )
+
+    def __str__(self):
+        return f" Order #{self.id} - {self.status.name if self.status else "No status"
+
+
