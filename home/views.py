20c0693cd7dfd3e41f178358b6b79dd02de271b4
surya21102003 <156424142+surya21102003@users.noreply.github.com>
2025-09-24 19:03:21 +0530

    50419

--- home/views.py
+++ home/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
-from rest_framework.generics import ListAPIview
-from .models imoprt MenuCategory
-from .serializers import MenuCategoryserializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from .modelsimport MenuItem
+from .serializers import MenuItemSerializer
 
+class MenuItemPagination(PageNumberPagination):
+    page_size=10
+    page_size_query_param='page_size'
+    max_page_size=100
 
-class MenuCategoryListView(ListAPIview):
-    querset=MenuCategory.objects.all()
-    serializers_class=MenuCategoryserializer
+class MenuItemViewSet(viewsets.viewset):
+    pagination_class=MenuItemPagination
+    del list(self,request):
+        query=request.query_params.get('search',None)
+        items=MenuItem.objects.all()
+        if query:
+            items=items.filter(name__icontains=query)
+            paginator=self.pagination_class()
+            page=paginator.paginate_queryset(items,request)
+            serializer=MenuItemSerializer(page,many=True)
+            paginator.get_paginated_response(serializer.data)    
 
-# Create your views here.
